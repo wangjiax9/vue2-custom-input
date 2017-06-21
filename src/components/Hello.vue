cd1b7a8bebc1c4a39a6a63e4090af0e171f42e8e
jialong w <jialong w>
2017-06-22 01:53:23 +0800

    custom input

---
+++ src/components/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ori-input v-model="val"></ori-input>
+    <h3>{{val}}</h3>
+  </div>
+</template>
+
+<script>
+import Input from './Input'
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      val:''
+    }
+  },
+  components:{
+    'ori-input':Input
+  }
+}
+</script>
+
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
